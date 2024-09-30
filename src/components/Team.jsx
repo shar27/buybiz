1139aef5a2577ecbbb077e2182b701a38a116063
Shar <81372629+shar27@users.noreply.github.com>
2024-09-30 11:12:27 +0200

    page development

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -4,17 +4,16 @@ export const Team = (props) => {
   return (
     <div id="team" className="text-center">
       <div className="container">
-        <div className="col-md-8 col-md-offset-2 section-title">
+        <div className="col-md-12 section-title">
           <h2>Meet the Team</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+           I am Shariq Ahmed, I have run various businesses throughout my career in sectors such as Trades, E-commerce & Technology.
           </p>
         </div>
         <div id="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                <div key={`${d.name}-${i}`} className="col-md-12 col-sm-12 team">
                   <div className="thumbnail">
                     {" "}
                     <img src={d.img} alt="..." className="team-img" />
